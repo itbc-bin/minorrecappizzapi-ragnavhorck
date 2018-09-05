81a6fc0133d63fcae9efe79408587c044d4d0702
Ragna <ragna.v.h@hotmail.com>
2018-09-05 14:47:52 +0200

    kan pizza's toevoegen dmv een post request

--- pizzaDB.py
+++ pizzaDB.py
@@ -3,13 +3,32 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-pizzaDB = [ {"name": "tonno"},
-            {"name": "salami"},
-            {"name": "fungi"}
+pizzaDB = [ {"name": "tonno", "ingredienten": ['tomaat','kaas', 'tonijn', 'oliif']},
+            {"name": "salami", "ingredienten": ['tomaat','kaas','salami']},
+            {"name": "fungi", "ingredienten": ['tomaat','kaas','ham','ananas']}
           ]
+
+
 @app.route("/", methods=['GET'])
 def geefPizza():
     return jsonify({"pizzaDB":pizzaDB})
+
+@app.route("/<string:name>", methods=['GET'])
+def getOnePizza(name):
+    resultPizza = []
+    for pizzaSmaak in pizzaDB:
+        if pizzaSmaak["name"] == name:
+            return str(pizzaSmaak)
+
+@app.route("/", methods=['POST'])
+def addOnePizza():
+    pizza = {'name': request.json['name']}
+    pizzaDB.append(pizza)
+    return jsonify({'pizzaDB': pizzaDB})
+
+
+    #return jsonify({"pizzaDB":resultPizza})
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
