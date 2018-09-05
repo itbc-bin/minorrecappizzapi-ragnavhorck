ffe2f584eeb6156c2f3e6d6b28435fc4ef6d7895
Ragna <ragna.v.h@hotmail.com>
2018-09-05 16:16:27 +0200

    put and delete methods toegevoegd

--- pizzaDB.py
+++ pizzaDB.py
@@ -56,5 +56,25 @@ def addPricePizza():
                 pizzaSmaak['prijs'] = pizzaPrijs
                 return jsonify({'pizzaDB': pizzaDB})
 
+
+@app.route("/<string:name>", methods={'PUT'})
+def putPizza(name):
+    resultPizza=[]
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            resultPizza.append(pizza)
+        resultPizza[0]['name'] = request.json['name']
+        return jsonify({'pizzaDB': pizzaDB})
+
+@app.route("/<string:name>", methods=["DELETE"])
+def delPizza(name):
+    resultPizza = []
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            resultPizza.append(pizza)
+    pizzaDB.remove(resultPizza[0])
+    return jsonify({'pizzaDB': pizzaDB})
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
